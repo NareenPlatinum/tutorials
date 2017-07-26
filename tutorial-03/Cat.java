79e497eeb1b2c6235508ce16a7497427b58cbb9e
Nareen <Nareen.Khurshid@carleton.ca>
2017-07-26 16:34:58 -0400

    update

--- tutorial-03/Cat.java
+++ tutorial-03/Cat.java
@@ -1,2 +1,14 @@
 public class Cat extends Animal{
+  
+	protected String name;
+	protected int    birthYear;
+	public String getName(){ return this.name; }
+	public int    getBirthyear(){ return this.birthYear; }
+  public abstract String noise();
+
+	public Animal(String name, int year){
+		this.name = name;
+		this.birthYear = year;
+	}
+  
 }
